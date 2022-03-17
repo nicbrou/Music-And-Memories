07a3e4a53972d3397359f88df4df70ad43619e01
Nicole Broughton <nbrought28@gmail.com>
2022-03-17 01:21:57 -0400

    Added background image to layout.ejs, signup.ejs, and signin.ejs. Styled various buttons, text and borders.

--- routes/saved.js
+++ routes/saved.js
@@ -2,23 +2,32 @@ const express = require("express");
 var methodOverride = require("method-override");
 const router = express.Router();
 router.use(methodOverride("_method"));
+const isLoggedIn = require("../helper/isLoggedIn");
 
 router.use(express.urlencoded({ extended: true }));
 
 const savedCntrl = require("../controllers/saved");
 
 // Save Playlist to Saved Page
-router.get("/user/saved", savedCntrl.saved_create_get);
-router.put("/user/saved/:id", savedCntrl.saved_create_put);
-router.put("/user/saved/:id", savedCntrl.saved_create_put);
+router.get("/user/saved", isLoggedIn, savedCntrl.saved_create_get);
+router.put("/user/saved/:id", isLoggedIn, savedCntrl.saved_create_put);
+router.put("/user/saved/:id", isLoggedIn, savedCntrl.saved_create_put);
 
 // Delete Playlist from Saved Page
-router.delete("/user/saved/delete/:id", savedCntrl.saved_create_delete);
+router.delete(
+  "/user/saved/delete/:id",
+  isLoggedIn,
+  savedCntrl.saved_create_delete
+);
 
 // Add Note to Saved Playlist
-router.post("/user/saved/add/:id", savedCntrl.saved_addNote_post);
+router.post("/user/saved/add/:id", isLoggedIn, savedCntrl.saved_addNote_post);
 
 // Delete Note from Saved Playlist
-router.delete("/user/saved/delete/notes/:id", savedCntrl.saved_addNote_delete);
+router.delete(
+  "/user/saved/delete/notes/:id",
+  isLoggedIn,
+  savedCntrl.saved_addNote_delete
+);
 
 module.exports = router;
